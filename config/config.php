d16d66e96f639c8d75dbf5208b23309935c37c38
Eliezer <PC@W10PC>
2018-11-30 00:11:19 -0200

    Update

--- config/config.php
+++ config/config.php
@@ -1,20 +1,20 @@
 <?php
 
-$PastaInterna = "PhpMvc";
+$PastaInterna = "";
 #Caminho Absoluto
-define('CAMINHO_PAGE', "http://{$_SERVER['HTTP_HOST']}/{$PastaInterna}");
+define('CAMINHO_PAGE', "https://{$_SERVER['HTTP_HOST']}/{$PastaInterna}");
 #Caminhos especificos
-define('CAMINHO_IMG', CAMINHO_PAGE."/public/img/");
-define('CAMINHO_CSS', CAMINHO_PAGE."/public/css/");
-define('CAMINHO_JS', CAMINHO_PAGE."/public/js/");
+define('CAMINHO_IMG', CAMINHO_PAGE."public_html/img/");
+define('CAMINHO_CSS', CAMINHO_PAGE."public_html/css/");
+define('CAMINHO_JS', CAMINHO_PAGE."public_html/js/");
 #Directore Raiz
-define('DIR_ROOT', "{$_SERVER['DOCUMENT_ROOT']}{$PastaInterna}");
+//define('DIR_ROOT', "{$_SERVER['DOCUMENT_ROOT']}{$PastaInterna}");
 
-//if (substr($_SERVER['DOCUMENT_ROOT'], -1) == '/') {
-//    define('DIR_ROOT', "{$_SERVER['DOCUMENT_ROOT']}{$PastaInterna}");
-//}else{
-//    define('DIR_ROOT', "{$_SERVER['DOCUMENT_ROOT']}/{$PastaInterna}");    
-//}
+if (substr($_SERVER['DOCUMENT_ROOT'], -1) == '/') {
+    define('DIR_ROOT', "{$_SERVER['DOCUMENT_ROOT']}{$PastaInterna}");
+}else{
+    define('DIR_ROOT', "{$_SERVER['DOCUMENT_ROOT']}/{$PastaInterna}");    
+}
 
 #Acesso Banco de Dados
 define('DB_HOST', "localhost");
--- public_html/index.php
+++ public_html/index.php
@@ -8,12 +8,14 @@ header("Content-Type: text/html;charset=utf-8");
 require_once '../vendor/autoload.php';
 require_once '../config/config.php';
 
-//echo CAMINHO_PAGE.'</BR>';
-//echo CAMINHO_IMG.'</BR>';
-//echo DIR_ROOT.'</BR>';
+echo CAMINHO_PAGE.'</BR>';
+echo CAMINHO_IMG.'</BR>';
+echo DIR_ROOT.'</BR>';
 
 //use App\teste;
 //$teste = new teste();
+
+//echo 'Hello Word';
 
 ?>
 <img src="<?php echo CAMINHO_IMG.'modelo6.png'; ?>">
